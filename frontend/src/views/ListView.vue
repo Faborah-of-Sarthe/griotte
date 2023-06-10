c8a8648510bc48b7a29f2f2363b41e247eebc725
Fwagra <fabiocerqueiracw@gmail.com>
2023-06-10 23:21:32 +0200

    API connection

--- frontend/src/views/ListView.vue
+++ frontend/src/views/ListView.vue
@@ -1,6 +1,22 @@
+
+<script setup>
+
+import { useQuery } from '@tanstack/vue-query'
+
+// Get sections and products from API
+const { isLoading, isError, data, error } = useQuery({
+  queryKey:  ['products'],
+  queryFn: () => {
+    return fetch(import.meta.env.VITE_API_URL + 'products').then((res) =>
+      res.json()
+    )
+  }
+})
+
+</script>
 <template>
   <div class="about">
-   LISTE
+
   </div>
 </template>
 
