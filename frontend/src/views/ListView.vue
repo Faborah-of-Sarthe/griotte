5b63daea50a6280ae3cfdc19c078bb0ed1cdea4f
Fwagra <fabiocerqueiracw@gmail.com>
2023-06-11 23:17:59 +0200

    Animate the sections

--- frontend/src/views/ListView.vue
+++ frontend/src/views/ListView.vue
@@ -2,14 +2,17 @@
 
 import { useQuery } from '@tanstack/vue-query'
 import Section from '@/components/Section.vue'
+import { ref } from 'vue'
+
+const items = ref([1,2,3,4,5,6,7,8,9,10])
 
 // Get sections and products from API
 const { isLoading, isError, data, error } = useQuery({
   queryKey:  ['products'],
-  queryFn: () => {
-    return fetch(import.meta.env.VITE_API_URL + 'products').then((res) =>
-      res.json()
-    )
+  queryFn: async () => {
+    const res = await fetch(import.meta.env.VITE_API_URL+'products');
+    return await res.json();
+
   }
 })
 
@@ -19,13 +22,17 @@ const { isLoading, isError, data, error } = useQuery({
     <div v-if="isLoading">Loading...</div>
     <div v-if="isError">Error: {{ error }}</div>
     <div v-if="data">
-      <div v-for="section in data" :key="section.id">
-          <Section :section="section"></Section>
-      </div>
+      <!-- Loop through example array with animation -->
+      <template v-for="section in data" :key="section.id">
+          <Transition name="slideIn" appear>
+            <Section :style="{ 'transition-delay': section.id * 50 + 'ms' }" :section="section"></Section>
+          </Transition>
+      </template>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 
-</style>
+
+</style> 
