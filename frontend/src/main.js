7f1a3d7994a0103fcc70f33890034e3629f71f57
Fwagra <fabiocerqueiracw@gmail.com>
2023-12-11 14:37:31 +0100

    Update the authentication mecanism to work with the last axios version

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -29,6 +29,7 @@ axios.interceptors.response.use(
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
 
 const pinia = createPinia()
