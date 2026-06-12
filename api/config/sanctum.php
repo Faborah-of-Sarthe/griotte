e4330fcbfc4c8223c773aac719aabbd73a869134
Fwagra <fabiocerqueiracw@gmail.com>
2026-06-12 10:21:09 +0200

    switch classes

--- api/config/sanctum.php
+++ api/config/sanctum.php
@@ -62,7 +62,7 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class,
+        'validate_csrf_token' => App\Http\Middleware\PreventRequestForgery::class,
     ],
 
 ];
